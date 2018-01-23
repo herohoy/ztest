71063b52a8ee0ecade8b01274d6bb25fe3e01052
lihui <herohoy@icloud.com>
2018-01-23 17:58:55 +0800

    some inns

--- src/main/scala/com/today36524/Main.scala
+++ src/main/scala/com/today36524/Main.scala
@@ -1,10 +1,16 @@
 package com.today36524
 
+import com.today36524.fibonacci.Fibonacci
 import com.today36524.print99.PrintNineNine
 
 object Main {
   def main(args: Array[String]): Unit = {
     val nn = new PrintNineNine
     nn.printNineNine()
+
+    println()
+
+    val fb = new Fibonacci
+    fb.fibonacci(50)
   }
 }
--- src/main/scala/com/today36524/fibonacci/Fibonacci.scala
+++ src/main/scala/com/today36524/fibonacci/Fibonacci.scala
@@ -1,8 +1,19 @@
 package com.today36524.fibonacci
 
+import scala.annotation.tailrec
+
 /**
   * 斐波那契数
   */
 class Fibonacci {
-  def fibonacci (n:Int) = ???
+
+
+  @tailrec
+  final def fibonacci(remain: Int, n1: Long = 1, n2: Long = 1): Long = {
+    remain match {
+      case 0 => println(n1);n1
+      case _ => println(n1)
+        fibonacci(remain - 1, n2, n1 + n2)
+    }
+  }
 }
